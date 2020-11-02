db31532ecf980983c1d72a80bf10f046d2cd98e1
AlexJS6 <alex.spiesberger@gmail.com>
2020-11-02 12:48:32 +0100

    Python learning tests

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-#https://www.codewars.com/kata/56dec885c54a926dcd001095
-def opposite(number):
-    return -number
+# -*- cofing:utf-8 -*
+
+
+def set_var(val):
+    """Fonction nous permettant de tester la portée 
+    des variables ds fonctions"""
+
+    try:
+        print('avant, notre var vaut: {}'.format(var))
+    except NameError:
+        print('Var n\'existe pas encore')
+    var = val
+    print('après, notre var vaut{}'.format(var))
+
+set_var(5)
+print(var)
