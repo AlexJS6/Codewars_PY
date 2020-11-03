e1de6f6b957fbde296d14e95293722475b700ea8
AlexJS6 <alex.spiesberger@gmail.com>
2020-11-03 23:09:28 +0100

    Adding kata 7

---
+++ kata_7.py
@@ -0,0 +1,8 @@
+#https://www.codewars.com/kata/54ff3102c1bad923760001f3
+def get_count(input_str):
+    num_vowels = 0
+    for letter in input_str:
+        if (letter.lower() == 'a' or letter.lower() == 'e' or letter.lower() == 'u' or letter.lower() == 'i' or letter.lower() == 'o'):
+            num_vowels += 1
+    
+    return num_vowels
--- test.py
+++ test.py
@@ -1,3 +1,18 @@
 # -*- cofing:utf-8 -*
 
 # https://www.w3schools.com/python/python_strings.asp check every data-strcture method (bottom of page here string)
+# Continue at 'Operators'
+
+def get_count(input_str):
+    num_vowels = 0
+    for letter in input_str:
+        if (letter.lower() == 'a' or letter.lower() == 'e' or letter.lower() == 'u' or letter.lower() == 'i' or letter.lower() == 'o'):
+            num_vowels += 1
+    
+    return num_vowels
+
+print(get_count("abracadabra"))#, 5)
+print(get_count(""))#, 0)
+print(get_count("pear tree"))#, 4)
+print(get_count("o a kak ushakov lil vo kashu kakao"))#, 13)
+#print(get_count("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"), 168)
