9c0140c8a474833cd62013fc68c04571b42be8ee
AlexJS6 <alex.spiesberger@gmail.com>
2020-11-03 23:31:59 +0100

    New kata 7

--- kata_7.py
+++ kata_7.py
@@ -6,6 +6,17 @@ def get_count(input_str):
             num_vowels += 1
     
     return num_vowels
-
+#optimized
 def getCount(inputStr):
    return sum(inputStr.count(char) for char in ['a', 'e', 'i', 'o', 'u'])
+
+
+
+#https://www.codewars.com/kata/56747fd5cb988479af000028/train/python
+from math import *
+def get_middle(s):
+    length = len(s)
+    if length % 2 == 0:
+        return s[int(length/2-1)] + s[int(length/2)]
+    else:
+        return s[floor(length/2)]
--- test.py
+++ test.py
@@ -2,17 +2,17 @@
 
 # https://www.w3schools.com/python/python_strings.asp check every data-strcture method (bottom of page here string)
 # Continue at 'Operators'
+from math import *
+def get_middle(s):
+    length = len(s)
+    if length % 2 == 0:
+        return s[int(length/2-1)] + s[int(length/2)]
+    else:
+        return s[floor(length/2)]
 
-def get_count(input_str):
-    num_vowels = 0
-    for letter in input_str:
-        if (letter.lower() == 'a' or letter.lower() == 'e' or letter.lower() == 'u' or letter.lower() == 'i' or letter.lower() == 'o'):
-            num_vowels += 1
-    
-    return num_vowels
 
-print(get_count("abracadabra"))#, 5)
-print(get_count(""))#, 0)
-print(get_count("pear tree"))#, 4)
-print(get_count("o a kak ushakov lil vo kashu kakao"))#, 13)
-#print(get_count("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"), 168)
+print(get_middle("test"))#,"es")
+print(get_middle("testing"))#,"t")
+print(get_middle("middle"))#,"dd")
+print(get_middle("A"))#,"A")
+print(get_middle("of"))#,"of")
