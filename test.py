dc2642a76d88bdc71a1b79fe5fc3dce8f341770b
AlexJS6 <alex.spiesberger@gmail.com>
2020-11-03 01:27:16 +0100

    slight justification

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 # -*- cofing:utf-8 -*
 
-# https://www.w3schools.com/python/python_strings.asp check every string method (bottom of page)
+# https://www.w3schools.com/python/python_strings.asp check every data-strcture method (bottom of page here string)
