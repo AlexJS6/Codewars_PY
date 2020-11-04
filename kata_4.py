5a8dae9818411062c7d9786bfb3f0de58fe346a7
AlexJS6 <alex.spiesberger@gmail.com>
2020-11-04 20:08:03 +0100

    Adding kata 4 in python

--- kata_4.py
+++ kata_4.py
@@ -47,3 +47,30 @@ def format_duration(num):
         return '{}, {}, {} and {}'.format(result[0], result[1], result[2], result[3])
     else :
         return '{}, {}, {}, {} and {}'.format(result[0], result[1], result[2], result[3], result[4])
+
+
+
+
+#https://www.codewars.com/kata/529bf0e9bdf7657179000008/train/python
+def validSolution(board):
+
+    correct = [1,2,3,4,5,6,7,8,9]
+    
+    for i in range(9):
+        if not row(i, board) == col(i, board) == square(i, board) == correct:
+            return False
+            
+    return True
+
+def row(i, board):
+    return sorted(board[i])
+
+def col(i, board):
+    return sorted(row[i] for row in board)
+
+def square(i, board):
+    
+    rows = [x + 3*(i // 3) for x in range(3)]
+    cols = [x + 3*(i %  3) for x in range(3)]        
+    
+    return sorted([board[x][y] for x in rows for y in cols])
