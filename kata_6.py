b2cb5281121beec03b83949722d39754ac168725
AlexJS6 <alex.spiesberger@gmail.com>
2020-11-04 13:45:54 +0100

    correction: kata was 6 not 7

---
+++ kata_6.py
@@ -0,0 +1,4 @@
+#https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
+def find_it(seq):
+        if seq.count(num) % 2 != 0 for num in seq:
+            return num
--- kata_7.py
+++ kata_7.py
@@ -60,8 +60,5 @@ def reverse_words(str):
 
 
 
-#https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
-def find_it(seq):
-        if seq.count(num) % 2 != 0 for num in seq:
-            return num
+
 
