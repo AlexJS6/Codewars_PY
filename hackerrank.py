2d6b12ab3e68e3c26dab57a4e5ae7fc26bcf847d
AlexJS6 <alex.spiesberger@gmail.com>
2020-11-27 13:53:07 +0100

    Adding hackerrank easy

--- hackerrank.py
+++ hackerrank.py
@@ -167,3 +167,12 @@ def checkMagazine(magazine, note):
     a = Counter(magazine)
     b = Counter(note)
     print("Yes" if a & b == b else "No")
+
+
+
+#https://www.hackerrank.com/challenges/two-strings/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps&h_r=next-challenge&h_v=zen
+def twoStrings(s1, s2):
+    for x in s1:
+        if x in s2:
+            return 'YES'
+    return 'NO'
--- test.py
+++ test.py
@@ -7,49 +7,17 @@
 #at for loops
 
 
-import os
-
-
-
-from collections import Counter
-
-
-def checkMagazine(magazine, note):
-    a = Counter(magazine)
-    b = Counter(note)
-    return "Yes" if a & b == b else "No"
-
-
-
-
-string = ['two', 'times', 'two', 'is', 'not', 'four']
-string2 = ['two', 'times', 'two', 'is', 'four']
-
-
-print(checkMagazine(string, string2))
-
-
-'''def checkMagazine(magazine, note):
-    #Count all words in magazine
-    #Counter({'two': 1, 'times': 1, 'three': 1, 'is': 1, 'not': 1, 'four': 1})
-    a = Counter(magazine)
-    
-    #Count all words in note
-    #Counter({'two': 2, 'times': 1, 'is': 1, 'four': 1})
-    b = Counter(note)
-    
-    # a & b - a intersection b
-    # Counter({'two': 1, 'times': 1, 'is': 1, 'four': 1})
-    return "Yes" if ( a & b ) == b else "No"
-
-m,n = map(int,input().split())
-magazine = input().split()
-note = input().split()
-print(checkMagazine(magazine, note))'''
+#import os
 
 
 
 
 
 
-os.system('pause')
+
+
+
+
+
+
+#os.system('pause')
