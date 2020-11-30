7c594d77b48eb82d51db20dd558f20868de2df16
AlexJS6 <alex.spiesberger@gmail.com>
2020-12-01 00:57:32 +0100

    Adding working solution with frequency tables

--- hackerrank.py
+++ hackerrank.py
@@ -247,3 +247,34 @@ def activityNotifications(expenditure, d):
             result += 1
         i += 1
     return result
+
+# This one is working using frequency tables:
+def activityNotifications(expenditure, d):
+    freq = {}
+    notify=0
+    def find(idx):
+        total_count = 0
+        for i in range(201): 
+            if i in freq:
+                total_count = total_count + freq[i]
+            if total_count >= idx:
+                return i
+    for i in range(len(expenditure)-1):
+        if expenditure[i] in freq:
+            freq[expenditure[i]]+=1
+        else:
+            freq[expenditure[i]]=1
+        #print(f"i: {i},val: {expenditure[i]}, freq: {freq}")
+        if i>=d-1:
+            if d%2 ==0:
+                median = (find(d//2)+find(d//2+1))/2
+            else:
+                median = find(d/2)
+            #print("median: ",median)
+            if expenditure[i+1]>= (median*2) :
+                notify +=1
+                print("notify: ",notify)
+            #remove the previous element from dictionary
+            freq[expenditure[i-d+1]]-=1
+
+    return notify  
