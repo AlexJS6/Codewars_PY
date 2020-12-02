3e7c489fa42d163de19ba7933183eacea3dfa078
AlexJS6 <alex.spiesberger@gmail.com>
2020-12-02 22:21:46 +0100

    Clean test.py

--- test.py
+++ test.py
@@ -13,36 +13,7 @@
 #find max valu
 #max vs sum
 
-from collections import Counter
 
-
-
-#https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
-def isValid(s):
-    #return Counter(s).most_common()
-    counts = Counter(map(lambda x:x[1], Counter(s).most_common()))
-    return counts
-    lcounts = sorted(counts.most_common())
-    #return lcounts
-    l = len(lcounts)
-    #return lcounts[0][0]+1, 1
-    if l == 2 and ((lcounts[0] == (1,1) or lcounts[1] == (lcounts[0][0]+1,1))):
-        return "YES"
-    return "YES" if l == 1 else "NO"
-
-    
-
-
-
-
-
-
-
-#print(isValid('aabbccddeefghi')) #NO
-#print(isValid('aabbcccc')) #NO #i = n
-print(isValid('ffiibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd')) #YES
-#print(isValid('aaaaaaabcc')) #YES
-#print(isValid('abcdfghhgfedecba')) #YES->NO
 
 
 
